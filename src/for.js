a1794acc34c4f14b7c55e6b67ba1e30c401e22ec
dentov13 <tovstokhatko.denis@gmail.com>
2016-12-16 19:30:44 +0200

    for ... of

---
+++ src/for.js
@@ -0,0 +1,11 @@
+let browsers = ['Chrome', 'FF', 'Edge', 'Safari', 'Opera'];
+
+for (let browser in browsers) {
+  console.log(browsers[browser]);
+}
+
+for (let browser of browsers) {
+  console.log(browser);
+}
+
+console.log(browsers);
