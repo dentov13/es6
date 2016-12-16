1dded141c1fe2ff0f414b503c20f9ea76b50741b
dentov13 <tovstokhatko.denis@gmail.com>
2016-12-16 16:47:38 +0200

    const

--- src/let.js
+++
@@ -1,9 +0,0 @@
-var btns = document.querySelectorAll('button');
-
-for (let i=0; i < btns.length; i++) {
-  var btn = btns[i];
-  btn.innerText = i;
-  btn.onclick = function (e) {
-    console.log(i);
-  }
-}
