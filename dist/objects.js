250aeb56af4016c74e58e290784573fc188e01cb
dentov13 <tovstokhatko.denis@gmail.com>
2016-12-16 21:16:50 +0200

    objects

---
+++ dist/objects.js
@@ -0,0 +1,47 @@
+"use strict";
+
+var fName = "Nick",
+    lName = "Lee",
+    email = "nick.lee@gmail.com";
+
+var Person = {
+  fName: fName,
+  lName: lName,
+  email: email,
+  sayHello: function sayHello() {
+    console.log("Hi, my name is " + fName);
+  },
+
+  get fullName() {
+    return this.fName + ' ' + this.lName;
+  },
+  set fullName(value) {
+    this.fName = value;
+  }
+};
+
+// Object.defineProperty(Person, 'fullname', {
+//   get: function() {
+//     return this.fName + " " + this.lName;
+//   },
+//   set: function(value) {
+//     this.fName = value;
+//   }
+// });
+
+console.log(Person);
+// Person.sayHello();
+//
+//
+//
+// function createCar(property, value) {
+//   return {
+//     [property]: value,
+//     ['_' + property]: value,
+//     [property.toUpperCase()]: value,
+//     ['get' + property]() {
+//       return this[property];
+//     }
+//   };
+// }
+// console.log(createCar('vin', 1));
